b179149dcbf206a63ec1c77812f0d88db02d6fd1
fiorafran <francofioravanti21@gmail.com>
2021-04-22 15:53:01 -0300

    auth, sign up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
+var myModal = new bootstrap.Modal(document.getElementById('modalRegistro'), {
+  keyboard: false
+});
 
-// document.querySelector('#btnIniciar').addEventListener('click', function(){
-//     console.log("Hola cómo andamo no me acuerdo de na!");
-// })
+const signUp = () => {
+	email = document.getElementById('registroCorreo').value;
+	password = document.getElementById('registroContraseña').value;
+
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+ 	.then((userCredential) => {
+    	// Signed in
+    	var user = userCredential.user;
+    	alert('Registro correcto '+ user);
+    	document.getElementById('registroCorreo').value = "";
+    	document.getElementById('registroContraseña').value = "";
+    	myModal.hide();
+  	})
+  	.catch((error) => {
+    	var errorCode = error.code;
+    	var errorMessage = error.message;
+    	alert(errorMessage+' '+errorCode);
+  	});
+}
 
 document.getElementById('btnIniciar').addEventListener('click', function(){
     console.log("Probando get element");
 });
+
+
+/*---------- REGISTRO ----------*/
+document.getElementById('btnRegistrar').addEventListener('click', signUp);
+
+document.getElementById('btnCancelar').addEventListener('click', ()=>{
+	document.getElementById('registroCorreo').value = "";
+	document.getElementById('registroContraseña').value = "";
+})
+
 
